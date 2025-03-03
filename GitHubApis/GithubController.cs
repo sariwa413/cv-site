8573fd5209460396f49c60407b97d44c5fd25c17
sariwa413 <159691592+sariwa413@users.noreply.github.com>
2025-03-03 19:52:50 +0200

    Add files via upload

---
+++ GitHubApis/GithubController.cs
@@ -0,0 +1,48 @@
+﻿using GithubService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace GitHubApis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+        private readonly IGitHubService _githubService;
+        private readonly string _githubToken;
+        public GithubController(IConfiguration configuration,IGitHubService githubService)
+        {
+            _githubService = githubService;
+            _githubToken = configuration["GithubToken"];
+        }
+
+        [Route("repositories")]
+        [HttpGet]
+        public async Task<ActionResult<List<Repository>>> SearchRepositories( [FromQuery] string? repoName, [FromQuery] string? lang,[FromQuery] string? userName)
+        {
+            var repos = await _githubService.SearchRepositories(repoName, lang, userName);
+
+            if (repos == null || repos.Count == 0)
+            {
+                return NotFound("No repositories found matching the criteria.");
+            }
+
+            return Ok(repos);
+        }
+
+
+        [Route("portfolio")]
+        [HttpGet]
+        public async Task<IActionResult> GetPortfolio()
+        {
+            var repos = await _githubService.GetPortfolio(_githubToken);
+            if (repos == null)
+            {
+                return NotFound();
+            }
+            return Ok(repos);
+        }
+    }
+}
+
